9a9b80d9c2ded380aa15672e63378d72527722c7
REINAEL <rbyabut@student,hau,edu,ph>
2024-02-07 23:28:53 +0800

    hello

---
+++ reinael quiz/backend/api/urls.py
@@ -0,0 +1,29 @@
+
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.getRoutes, name="routes"),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/profile/', views.getUserProfile, name='users-profile'),
+    path('users/', views.getUserProfile, name='users-profiles'),
+    path('users/register/', views.registerUser, name='register'),
+    path('products/', views.getProducts, name="products"),
+    path('products/<str:pk>', views.getProduct, name="products"),
+    path('shipping/', views.create_shipping_address, name="ship"),
+
+]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL,
+                                        document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, 
+                                       document_root=settings.MEDIA_ROOT)
+
+
+    # create_shipping_address
+
+
